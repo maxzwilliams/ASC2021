3870c1c5749102d8b70e617d1a83dea2d99f8034
Max <maxzwilliams2000@gmail.com>
2021-09-24 14:46:54 +1000

    polar coding begins

---
+++ codes/polarConvection.py
@@ -0,0 +1,15 @@
+"""
+Simulation of convection on a disk with a hole in the center
+
+Written by Maximilian Williams
+"""
+import matplotlib.pyplot as plt
+import time
+import copy
+import math
+
+
+
+
+
+
