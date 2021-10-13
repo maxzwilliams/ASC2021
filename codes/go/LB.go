6728075da6a562f0ca4a2d50f2c1133a185a594c
Max <maxzwilliams2000@gmail.com>
2021-10-14 09:10:14 +1100

    LB time

--- codes/go/LB.go
+++ codes/go/LB.go
@@ -5,6 +5,34 @@ package main
 import "fmt"
 
 
+
+func ss() int{
+	int n = 1
+	return n
+}
+
+type 2DField struct{
+	entries [][][]float64 // orientation, x coorindate, y coorindate
+	name string // name of the field
+}
+
+type 3DField struct{
+	entries [][][][]float64 // orientation, x coordinate, y coorindate, z coordinate
+	name string // name of the field
+}
+
+
+type lattice struct{
+	
+}
+
+func streamingStep2D(field 2DField, l lattice) 2DField{
+	for orientation 
+	field.entries[]
+}
+
+
+
 func main(){
 	fmt.Println("Hello World!")
 }
